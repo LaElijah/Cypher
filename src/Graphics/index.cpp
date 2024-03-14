4d5a56de507a5581c6139463088b9307d3f6fcd4
Elijah Allotey <elijah.allotey.w@gmail.com>
2024-03-14 10:44:42 -0500

    feat: made multiple objects appear with transformations

--- src/Graphics/index.cpp
+++ src/Graphics/index.cpp
@@ -78,7 +78,19 @@ int main()
     float visibility = 0.5f;
 
 
+    glm::vec3 cubePositions[] = {
+      glm::vec3( 0.0f, 0.0f, 0.0f),
+glm::vec3( 2.0f, 5.0f, -15.0f),
+glm::vec3(-1.5f, -2.2f, -2.5f),
+glm::vec3(-3.8f, -2.0f, -12.3f),
+glm::vec3( 2.4f, -0.4f, -3.5f),
+glm::vec3(-1.7f, 3.0f, -7.5f),
+glm::vec3( 1.3f, -2.0f, -2.5f),
+glm::vec3( 1.5f, 2.0f, -2.5f),
+glm::vec3( 1.5f, 0.2f, -1.5f),
+glm::vec3(-1.3f, 1.0f, -1.5f)
 
+    };
 
 
 
@@ -240,7 +252,7 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
       
-    model = glm::rotate(model, ((float) visibility) * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+   // model = glm::rotate(model, ((float) visibility) * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 
      
     unsigned int modelLocation = glGetUniformLocation(shaderProgram.ID, "model");
@@ -249,7 +261,6 @@ int main()
     unsigned int projectionLocation = glGetUniformLocation(shaderProgram.ID, "projection");
     
 
-    glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model)); 
     glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
     glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, glm::value_ptr(projection));
    
@@ -263,8 +274,21 @@ int main()
 
     glBindVertexArray(VAOs[0]);
 
+for(unsigned int i = 0; i < 10; i++)
+{
+  glm::mat4 model = glm::mat4(1.0f);
+
+    model = glm::translate(model, cubePositions[i]);
+
+    float angle = 20.0f * i;
+    model = glm::rotate(model, glm::radians(angle),
+    glm::vec3(1.0f, 0.3f, 0.5f));
+
+    glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model)); 
+
     glDrawArrays(GL_TRIANGLES, 0, 36);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
+
 
 
 
